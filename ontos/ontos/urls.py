cdc63f5c807a9bf7317a5270ca7f79e7dcabb9be
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2014-09-06 11:20:43 -0700

    urls.py

--- ontos/ontos/urls.py
+++ ontos/ontos/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from ontos.ontosApp import views
 
 from django.contrib import admin
 admin.autodiscover()
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +14,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
+
