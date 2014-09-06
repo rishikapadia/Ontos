91568bab340387184a78472be69590f7ba5cb69c
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2014-09-06 12:11:08 -0700

    db synced

--- ontos/ontos/urls.py
+++ ontos/ontos/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^', ),
+    #url(r'^', ),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
