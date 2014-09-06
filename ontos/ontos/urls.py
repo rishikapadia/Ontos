af69106ad9925365b65f9a7d176d9c0a41339f61
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2014-09-06 11:55:27 -0700

    need to update urls.py for main page

--- ontos/ontos/urls.py
+++ ontos/ontos/urls.py
@@ -15,7 +15,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     
+    url(r'^', ),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
 
