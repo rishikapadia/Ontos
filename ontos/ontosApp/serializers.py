58b866b270975a14e62191e2e8a714807c776728
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2014-09-06 11:47:57 -0700

    models.py updated

--- ontos/ontosApp/serializers.py
+++ ontos/ontosApp/serializers.py
@@ -5,5 +5,5 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('firstName', 'lastName', 'username', 'email', 'notes', 'top10hits')
+        fields = ('firstName', 'lastName', 'username', 'email', 'top10hits', 'metadata')
 
